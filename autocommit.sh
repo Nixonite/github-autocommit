a201400fac27174508842cede141d7844966dddc
Nixonite <programminglinguist@gmail.com>
2020-01-18 20:40:16 +0000

    bump

--- autocommit.sh
+++ autocommit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm my_data_file.txt
-shuf -i 0-9999999 -n 3 > $(PWD)/my_data_file.txt
+shuf -i 0-9999999 -n 3 > /home/ubuntu/github-autocommit/my_data_file.txt
 git add -A
 git commit -am "bump"
 git push origin master
